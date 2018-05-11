3ca8593a230f6e4e0d7c33ddb62bfc9b8f95a759
Unknown <EBoblett949@west-mec.org>
2018-05-11 15:27:33 -0700

    Started form validation.
    Started form validation.

--- contact.js
+++ contact.js
@@ -1,3 +1,4 @@
+/*media queries*/
 function myFunction(size) {
     if (size.matches) { // If media query matches
         document.body.style.backgroundColor = ""/*add a style here*/;
@@ -9,3 +10,22 @@ function myFunction(size) {
 var size = window.matchMedia("(max-width: 700px)")
 myFunction(size) // Call listener function at run time
 size.addListener(myFunction) // Attach listener function on state changes
+
+
+/*forms?*/
+document.getElementById("sub").onclick = function() {
+  validate();
+}
+
+function validate() {
+  var name = document.getElementById("name");
+  var email = document.getElementById("email");
+
+  if (name = "") {
+    alert("Please enter your name.");
+  }
+
+  if (email = "") {
+    alert("Please enter your email.");
+  }
+}
